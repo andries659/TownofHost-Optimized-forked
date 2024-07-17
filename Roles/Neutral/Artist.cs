b97e46d4688db8b2764b56c9f777b93f1b55b6ca
Ape <155788660+ApeMV@users.noreply.github.com>
2024-07-17 14:03:17 +0200

    Fix a lot of Artist errors

--- Roles/Neutral/Artist.cs
+++ Roles/Neutral/Artist.cs
@@ -2,134 +2,125 @@ using AmongUs.GameOptions;
 using static TOHE.Options;
 using static TOHE.Translator;
 
-namespace TOHE.Roles.Neutral
+namespace TOHE.Roles.Neutral;
+
+internal class Artist : RoleBase
 {
-    internal class Artist : RoleBase
+    
+    private static readonly NetworkedPlayerInfo.PlayerOutfit PaintedOutfit = new NetworkedPlayerInfo.PlayerOutfit()
+        .Set("", 15, "", "", "visor_Crack", "", "");
+
+    private static readonly Dictionary<byte, NetworkedPlayerInfo.PlayerOutfit> OriginalPlayerSkins = new();
+
+    private const int Id = 12889;
+    private static readonly HashSet<byte> PlayerIds = new();
+    public static bool HasEnabled => PlayerIds.Any();
+
+    public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralKilling;
+    private static OptionItem HideNameOfPaintedPlayer;
+    private static OptionItem KillCooldown;
+    private static OptionItem PaintCooldown;
+    private static OptionItem CanVent;
+    private static OptionItem HasImpostorVision;
+
+    private static readonly Dictionary<byte, float> NowCooldown = new();
+    private static readonly Dictionary<byte, List<byte>> PlayerSkinsPainted = new();
+    
+    public override void SetupCustomOption()
     {
-        private static readonly NetworkedPlayerInfo.PlayerOutfit PaintedOutfit = new NetworkedPlayerInfo.PlayerOutfit()
-            .Set("", 15, "", "", "visor_Crack", "", "");
+        SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Artist);
+        KillCooldown = FloatOptionItem.Create(Id + 10, GeneralOption.KillCooldown, new(0f, 180f, 2.5f), 30f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
+            .SetValueFormat(OptionFormat.Seconds);
+        PaintCooldown = FloatOptionItem.Create(Id + 11, "PaintCooldown", new(0f, 180f, 2.5f), 5f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
+            .SetValueFormat(OptionFormat.Seconds);
+        HideNameOfPaintedPlayer = BooleanOptionItem.Create(Id + 12, "ArtistHideNamePainted", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
+        CanVent = BooleanOptionItem.Create(Id + 12, GeneralOption.CanVent, true, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
+        HasImpostorVision = BooleanOptionItem.Create(Id + 13, GeneralOption.ImpostorVision, true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
+    }
 
-        private static readonly Dictionary<byte, NetworkedPlayerInfo.PlayerOutfit> OriginalPlayerSkins = new();
+    public override void Init()
+    {
+        PlayerSkinsPainted.Clear();
+        OriginalPlayerSkins.Clear();
+         PlayerIds.Clear();
+    }
 
-        private const int Id = 12889;
-        private static readonly HashSet<byte> PlayerIds = new();
-        public static bool HasEnabled => PlayerIds.Any();
 
-        public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
-        public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralKilling;
+    public override void Add(byte playerId)
+    {
+         PlayerIds.Add(playerId);
 
-        private static OptionItem KillCooldown;
-        private static OptionItem PaintCooldown;
-        private static OptionItem CanVent;
-        private static OptionItem HasImpostorVision;
-        private static OptionItem HideNameOfPaintedPlayer;
+        var pc = Utils.GetPlayerById(playerId);
+        pc.AddDoubleTrigger();
 
-        private static readonly Dictionary<byte, float> NowCooldown = new();
-        private static readonly Dictionary<byte, List<byte>> PlayerSkinsPainted = new();
+         if (!Main.ResetCamPlayerList.Contains(playerId))
+            Main.ResetCamPlayerList.Add(playerId);
+     
+    }
+    public override void SetKillCooldown(byte id) 
+        => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
 
-        public override void SetupCustomOption()
+    public override void ApplyGameOptions(IGameOptions opt, byte id) 
+        => opt.SetVision(HasImpostorVision.GetBool());
+
+    public override bool CanUseKillButton(PlayerControl pc) => true;
+
+    public override bool CanUseImpostorVentButton(PlayerControl pc) => CanVent.GetBool();
+
+    private void SetPainting(PlayerControl killer, PlayerControl target)
+    {
+        if (!PlayerSkinsPainted[killer.PlayerId].Contains(target.PlayerId))
         {
-            SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Artist);
-            KillCooldown = FloatOptionItem.Create(Id + 10, GeneralOption.KillCooldown, new(0f, 180f, 2.5f), 30f, TabGroup.CrewmateRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
-                .SetValueFormat(OptionFormat.Seconds);
-            PaintCooldown = FloatOptionItem.Create(Id + 11, GeneralOption.PaintCooldown, new(0f, 180f, 2.5f), 5f, TabGroup.CrewmateRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
-                .SetValueFormat(OptionFormat.Seconds);
-            HideNameOfPaintedPlayer = BooleanOptionItem.Create(Id + 12, "ArtistHideNamePainted", true, TabGroup.CrewmateRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
+            SetSkin(target, PaintedOutfit);
         }
 
-        public override void Init()
-        {
-            PlayerSkinsPainted.Clear();
-            OriginalPlayerSkins.Clear();
-            PlayerIds.Clear();
-        }
+        PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
+        killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("ArtistPaintedSkin")));
+        target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("PaintedByArtist")));
 
-        public override void Add(byte playerId)
-        {
-            if (!PlayerSkinsPainted.ContainsKey(playerId))
-            {
-                PlayerSkinsPainted[playerId] = new List<byte>();
-            }
-            PlayerIds.Add(playerId);
-        }
+        OriginalPlayerSkins[target.PlayerId] = Camouflage.PlayerSkins[target.PlayerId];
+        Camouflage.PlayerSkins[target.PlayerId] = PaintedOutfit;
 
-        public override void AddPlayer(byte playerId)
-        {
-            PlayerIds.Add(playerId);
+        SendRPC(killer.PlayerId, target.PlayerId);
+        killer.SetKillCooldown();
+    }
 
-            var pc = Utils.GetPlayerById(playerId);
-            pc.AddDoubleTrigger();
+    private static void SetSkin(PlayerControl target, NetworkedPlayerInfo.PlayerOutfit outfit)
+    {
+        var sender = CustomRpcSender.Create(name: $"Artist.RpcSetSkin({target.Data.PlayerName})");
 
-            if (!Main.ResetCamPlayerList.Contains(playerId))
-                Main.ResetCamPlayerList.Add(playerId);
-        }
+        target.SetColor(outfit.ColorId);
+        sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetColor)
+            .Write(target.Data.NetId)
+            .Write((byte)outfit.ColorId)
+            .EndRpc();
 
-        public override void SetKillCooldown(byte id) 
-            => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
+        target.SetHat(outfit.HatId, outfit.ColorId);
+        sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetHatStr)
+            .Write(outfit.HatId)
+            .Write(target.GetNextRpcSequenceId(RpcCalls.SetHatStr))
+            .EndRpc();
 
-        public override void ApplyGameOptions(IGameOptions opt, byte id) 
-            => opt.SetVision(HasImpostorVision.GetBool());
-
-        public override bool CanUseKillButton(PlayerControl pc) => true;
-
-        public override bool CanUseImpostorVentButton(PlayerControl pc) => CanVent.GetBool();
-
-        private void SetPainting(PlayerControl killer, PlayerControl target)
-        {
-            if (!PlayerSkinsPainted[killer.PlayerId].Contains(target.PlayerId))
-            {
-                SetSkin(target, PaintedOutfit);
-            }
-
-            PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
-            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("ArtistPaintedSkin")));
-            target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("PaintedByArtist")));
-
-            OriginalPlayerSkins[target.PlayerId] = Camouflage.PlayerSkins[target.PlayerId];
-            Camouflage.PlayerSkins[target.PlayerId] = PaintedOutfit;
-
-            SendRPC(killer.PlayerId, target.PlayerId);
-            killer.SetKillCooldown();
-        }
-
-        private static void SetSkin(PlayerControl target, NetworkedPlayerInfo.PlayerOutfit outfit)
-        {
-            var sender = CustomRpcSender.Create(name: $"Artist.RpcSetSkin({target.Data.PlayerName})");
-
-            target.SetColor(outfit.ColorId);
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetColor)
-                .Write(target.Data.NetId)
-                .Write((byte)outfit.ColorId)
-                .EndRpc();
-
-            target.SetHat(outfit.HatId, outfit.ColorId);
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetHatStr)
-                .Write(outfit.HatId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetHatStr))
-                .EndRpc();
-
-            target.SetSkin(outfit.SkinId, outfit.ColorId);
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetSkinStr)
-                .Write(outfit.SkinId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetSkinStr))
-                .EndRpc();
+        target.SetSkin(outfit.SkinId, outfit.ColorId);
+        sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetSkinStr)
+            .Write(outfit.SkinId)
+            .Write(target.GetNextRpcSequenceId(RpcCalls.SetSkinStr))
+            .EndRpc();
 
             target.SetVisor(outfit.VisorId, outfit.ColorId);
             sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetVisorStr)
-                .Write(outfit.VisorId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetVisorStr))
-                .EndRpc();
+            .Write(outfit.VisorId)
+            .Write(target.GetNextRpcSequenceId(RpcCalls.SetVisorStr))
+            .EndRpc();
 
-            target.SetPet(outfit.PetId);
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetPetStr)
-                .Write(outfit.PetId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetPetStr))
-                .EndRpc();
+        target.SetPet(outfit.PetId);
+        sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetPetStr)
+            .Write(outfit.PetId)
+            .Write(target.GetNextRpcSequenceId(RpcCalls.SetPetStr))
+            .EndRpc();
 
-            sender.SendMessage();
-        }
+        sender.SendMessage();
     }
+    
 }
