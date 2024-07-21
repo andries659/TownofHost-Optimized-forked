6221d18b2cfe39aa7bcedbaec8586d07cebbff81
AndriesWorks <168019743+andries659@users.noreply.github.com>
2024-07-21 04:29:32 +0200

    Updated DarkFairy.cs

--- Roles/Neutral/DarkFairy.cs
+++ Roles/Neutral/DarkFairy.cs
@@ -10,100 +10,108 @@ internal class DarkFairy : RoleBase
     //===========================SETUP================================\\
     private const int Id = 29100;
     public static bool HasEnabled => CustomRoleManager.HasEnabled(CustomRoles.DarkFairy);
-    public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
+    public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralEvil;
     //==================================================================\\
 
-    private static OptionItem RememberCooldown;
-    private static OptionItem TaskCharmCooldown;
-    private static OptionItem CanVent;
+    private static OptionItem ConvertCooldown;
+    private static OptionItem ConvertCooldownIncrese;
+    private static OptionItem ConvertMax;
+    private static OptionItem KnowTargetRole;
+    private static OptionItem FairiesKnowEachOther;
+    private static OptionItem CanConvertNeutral;
+    public static OptionItem ConvertedCountMode;
+
+    private enum CharmedCountModeSelectList
+    {
+        DarkFairy_ConvertedCountMode_None,
+        DarkFairy_ConvertedCountMode_DarkFairy,
+        DarkFairy_ConvertedCountMode_Original
+    }
 
     public override void SetupCustomOption()
     {
-        SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.DarkFairy);
-        RememberCooldown = FloatOptionItem.Create(Id + 10, "RememberCooldown", new(0f, 180f, 2.5f), 25f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy])
-                .SetValueFormat(OptionFormat.Seconds);
-        TaskCharmCooldown = FloatOptionItem.Create(Id + 10, "TaskCharmCooldown", (0f, 180f, 2.5f), 20f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy])
+        SetupSingleRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.DarkFairy, 1, zeroOne: false);
+        ConvertCooldown = FloatOptionItem.Create(Id + 10, "DarkFairyCharmCooldown", new(0f, 180f, 2.5f), 30f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy])
             .SetValueFormat(OptionFormat.Seconds);
-        CanVent = BooleanOptionItem.Create(Id + 11, GeneralOption.CanVent, true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy]);
+        ConvertCooldownIncrese = FloatOptionItem.Create(Id + 11, "DarkFairyCharmCooldownIncrese", new(0f, 180f, 2.5f), 10f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy])
+            .SetValueFormat(OptionFormat.Seconds);
+        ConvertMax = IntegerOptionItem.Create(Id + 12, "DarkFairyCharmMax", new(1, 15, 1), 15, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy])
+            .SetValueFormat(OptionFormat.Times);
+        KnowTargetRole = BooleanOptionItem.Create(Id + 13, "DarkFairyKnowTargetRole", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy]);
+        FairiesKnowEachOther = BooleanOptionItem.Create(Id + 14, "DarkFairyFairiesKnowEachOther", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy]);
+        ConvertedCountMode = StringOptionItem.Create(Id + 17, "DarkFairy_CharmedCountMode", EnumHelper.GetAllNames<CharmedCountModeSelectList>(), 1, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy]);
+        CanConvertNeutral = BooleanOptionItem.Create(Id + 18, "DarkFairyCanCharmNeutral", false, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.DarkFairy]);
     }
     public override void Add(byte playerId)
     {
-        AbilityLimit = 1;
+        AbilityLimit = ConvertMax.GetInt();
 
-        if (!AmongUsClient.Instance.AmHost) return;
         if (!Main.ResetCamPlayerList.Contains(playerId))
             Main.ResetCamPlayerList.Add(playerId);
     }
-    public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = AbilityLimit >= 1 ? RememberCooldown.GetFloat() : 300f;
-    public override bool CanUseKillButton(PlayerControl player) => !player.Data.IsDead && (AbilityLimit > 0);
-    public override bool OnCheckMurderAsKiller(PlayerControl killer, PlayerControl target)
+    public override void SetConvertCooldown(byte id) => Main.AllPlayerKillCooldown[id] = AbilityLimit >= 1 ? ConvertCooldown.GetFloat() + (ConvertMax.GetInt() - AbilityLimit) * ConvertCooldownIncrese.GetFloat() : 300f;
+    public override bool OnCheckMurderAsKiller(PlayerControl target)
     {
         if (AbilityLimit < 1) return false;
-
-        var role = target.GetCustomRole();
-
-        if (role is CustomRoles.Jackal
-            or CustomRoles.HexMaster
-            or CustomRoles.Poisoner
-            or CustomRoles.Juggernaut 
-            or CustomRoles.BloodKnight
-            or CustomRoles.Sheriff)
+        if (Mini.Age < 18 && (target.Is(CustomRoles.NiceMini) || target.Is(CustomRoles.EvilMini)))
+        else if (CanBeConverted(target) && Mini.Age == 18 || CanBeConverted(target) && Mini.Age < 18 && !(target.Is(CustomRoles.NiceMini) || target.Is(CustomRoles.EvilMini)))
         {
             AbilityLimit--;
             SendSkillRPC();
-            killer.RpcSetCustomRole(role);
-            killer.GetRoleClass().OnAdd(killer.PlayerId);
+            target.RpcSetCustomRole(CustomRoles.Converterd);
 
-            if (role.IsCrewmate())
-                killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("RememberedCrewmate")));
-            else
-                killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("RememberedNeutralKiller")));
+            target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("ConvertedByDarkFairy")));
+            
+            Utils.NotifyRoles(SpecifySeer: killer, SpecifyTarget: target, ForceLoop: true);
+            Utils.NotifyRoles(SpecifySeer: target, SpecifyTarget: killer, ForceLoop: true);
 
-            // Notify target
-            target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("ImitatorImitated")));
-        }
-        else if (role.IsCrewmate())
-        {
-            AbilityLimit--;
-            SendSkillRPC();
-            killer.RpcSetCustomRole(CustomRoles.Sheriff);
-            killer.GetRoleClass().OnAdd(killer.PlayerId);
-            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("RememberedCrewmate")));
-            target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("ImitatorImitated")));
-        }
-        else if (role.IsImpostor())
-        {
-            AbilityLimit--;
-            SendSkillRPC();
-            killer.RpcSetCustomRole(CustomRoles.Refugee);
-            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("RememberedImpostor")));
-            target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("ImitatorImitated")));
-        }
-
-        var killerRole = killer.GetCustomRole();
-
-        if (killerRole != CustomRoles.DarkFairy)
-        {
             killer.ResetKillCooldown();
-            killer.SetKillCooldown(forceAnime: true);
+            killer.SetKillCooldown();
+            if (!DisableShieldAnimations.GetBool()) killer.RpcGuardAndKill(target);
+            target.RpcGuardAndKill(killer);
+            target.RpcGuardAndKill(target);
 
-            Logger.Info("Imitator remembered: " + target?.Data?.PlayerName + " = " + target.GetCustomRole().ToString(), "Imitator Assign");
-            Logger.Info($"{killer.GetNameWithRole()} : {AbilityLimit} remember limits left", "Imitator");
-
-            Utils.NotifyRoles(SpecifySeer: killer);
+            Logger.Info("设置职业:" + target?.Data?.PlayerName + " = " + target.GetCustomRole().ToString() + " + " + CustomRoles.Charmed.ToString(), "Assign " + CustomRoles.Charmed.ToString());
+            Logger.Info($"{killer.GetNameWithRole()} : 剩余{AbilityLimit}次魅惑机会", "Cultist");
+            return false;
         }
-        else if (killerRole == CustomRoles.DarkFairy)
+        killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Cultist), GetString("CultistInvalidTarget")));
+        Logger.Info($"{killer.GetNameWithRole()} : 剩余{AbilityLimit}次魅惑机会", "Cultist");
+        return false;
+    }
+    public static bool TargetKnowOtherTargets => TargetKnowOtherTarget.GetBool();
+    public static bool KnowRole(PlayerControl player, PlayerControl target)
+    {
+        if (player.Is(CustomRoles.Charmed) && target.Is(CustomRoles.Cultist)) return true;
+
+        if (KnowTargetRole.GetBool())
         {
-            killer.SetKillCooldown(forceAnime: true);
-            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.DarkFairy), GetString("ImitatorInvalidTarget")));
+            if (player.Is(CustomRoles.Cultist) && target.Is(CustomRoles.Charmed)) return true;
+            if (TargetKnowOtherTarget.GetBool() && player.Is(CustomRoles.Charmed) && target.Is(CustomRoles.Charmed)) return true;
         }
-
+        return false;
+    }
+    public override string GetProgressText(byte playerid, bool cooms) => Utils.ColorString(AbilityLimit >= 1 ? Utils.GetRoleColor(CustomRoles.Cultist).ShadeColor(0.25f) : Color.gray, $"({AbilityLimit})");
+    public static bool CanBeCharmed(PlayerControl pc)
+    {
+        return pc != null && (pc.GetCustomRole().IsCrewmate() || pc.GetCustomRole().IsImpostor() || 
+            (CanCharmNeutral.GetBool() && pc.GetCustomRole().IsNeutral())) && !pc.Is(CustomRoles.Charmed) 
+            && !pc.Is(CustomRoles.Admired) && !pc.Is(CustomRoles.Loyal) && !pc.Is(CustomRoles.Infectious) 
+            && !pc.Is(CustomRoles.Virus) && !pc.Is(CustomRoles.Cultist)
+            && !(pc.GetCustomSubRoles().Contains(CustomRoles.Hurried) && !Hurried.CanBeConverted.GetBool());
+    }
+    public static bool NameRoleColor(PlayerControl seer, PlayerControl target)
+    {
+        if (seer.Is(CustomRoles.Charmed) && target.Is(CustomRoles.Cultist)) return true;
+        if (seer.Is(CustomRoles.Cultist) && target.Is(CustomRoles.Charmed)) return true;
+        if (seer.Is(CustomRoles.Charmed) && target.Is(CustomRoles.Charmed) && TargetKnowOtherTarget.GetBool()) return true;
+        
         return false;
     }
     public override void SetAbilityButtonText(HudManager hud, byte playerId)
     {
-        hud.KillButton.OverrideText(GetString("ImitatorKillButtonText"));
+        hud.KillButton.OverrideText(GetString("CultistKillButtonText"));
     }
-
+    public override Sprite GetKillButtonSprite(PlayerControl player, bool shapeshifting) => CustomButton.Get("Subbus");
 }
