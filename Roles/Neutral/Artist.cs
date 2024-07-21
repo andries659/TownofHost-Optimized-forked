1a32f5ac5581018bbaf2cce9bc66f0a3d2ddd41d
Dailyhare <156022382+Dailyhare@users.noreply.github.com>
2024-07-20 19:16:31 -0700

    Update Artist.cs

--- Roles/Neutral/Artist.cs
+++ Roles/Neutral/Artist.cs
@@ -46,6 +46,14 @@ namespace TOHE.Roles.Neutral
                 .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
         }
 
+
+        public override void ApplyGameOptions(IGameOptions opt, byte id) => opt.SetVision(HasImpostorVision.GetBool());
+        public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
+        public override bool CanUseKillButton(PlayerControl pc) => true;
+        public override bool CanUseImpostorVentButton(PlayerControl pc) => CanVent.GetBool();
+        public override bool CanUseSabotage(PlayerControl pc) => false;
+
+
         public override void Init()
         {
             PlayerSkinsPainted.Clear();
