95d91fc508a409257edb0e84ec3a16fa03864c45
Dailyhare <156022382+Dailyhare@users.noreply.github.com>
2024-07-20 19:46:15 -0700

    Update Artist.cs

--- Roles/Neutral/Artist.cs
+++ Roles/Neutral/Artist.cs
@@ -1,4 +1,7 @@
 using AmongUs.GameOptions;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using static TOHE.Options;
 using static TOHE.Translator;
 using static TOHE.Utils;
@@ -10,25 +13,21 @@ namespace TOHE.Roles.Neutral
         private static readonly NetworkedPlayerInfo.PlayerOutfit PaintedOutfit = new NetworkedPlayerInfo.PlayerOutfit()
             .Set("", 15, "", "", "visor_Crack", "", "");
 
-        private static readonly Dictionary<byte, NetworkedPlayerInfo.PlayerOutfit> OriginalPlayerSkins = new();
-        
         private const int Id = 28800;
-        private static readonly HashSet<byte> PlayerIds = new();
-        public static bool HasEnabled => PlayerIds.Any();
+        private static readonly HashSet<byte> PlayerIds = new HashSet<byte>();
+        private static OptionItem KillCooldown;
+        private static OptionItem PaintCooldown;
+        private static OptionItem CanVent;
+        private static OptionItem HasImpostorVision;
+
+        private static readonly Dictionary<byte, float> NowCooldown = new Dictionary<byte, float>();
+        private static readonly Dictionary<byte, List<byte>> PlayerSkinsPainted = new Dictionary<byte, List<byte>>();
+        private readonly Dictionary<byte, int> KillClickCount = new Dictionary<byte, int>();
 
         public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
         public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralKilling;
 
-        private static OptionItem KillCooldown;
-        private static OptionItem HideNameOfPaintedPlayer;
-        private static OptionItem PaintCooldown;
-        private static OptionItem CanVent;
-        private static OptionItem HasImpostorVision;
-        private static OptionItem AbilityUses;
-
-        private static readonly Dictionary<byte, float> NowCooldown = new();
-        private static readonly Dictionary<byte, List<byte>> PlayerSkinsPainted = new();
-        private readonly Dictionary<byte, int> KillClickCount = new();
+        public static bool HasEnabled => PlayerIds.Any();
 
         public override void SetupCustomOption()
         {
@@ -39,20 +38,15 @@ namespace TOHE.Roles.Neutral
             PaintCooldown = FloatOptionItem.Create(Id + 11, "ArtistPaintCooldown", new(0f, 180f, 2.5f), 20f, TabGroup.NeutralRoles, false)
                 .SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
                 .SetValueFormat(OptionFormat.Seconds);
-            HideNameOfPaintedPlayer = BooleanOptionItem.Create(Id + 12, "ArtistHideNamePainted", true, TabGroup.NeutralRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
             CanVent = BooleanOptionItem.Create(Id + 13, GeneralOption.CanVent, true, TabGroup.NeutralRoles, false)
                 .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
             HasImpostorVision = BooleanOptionItem.Create(Id + 14, GeneralOption.ImpostorVision, true, TabGroup.NeutralRoles, false)
                 .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
         }
 
-
-        
         public override void Init()
         {
             PlayerSkinsPainted.Clear();
-            OriginalPlayerSkins.Clear();
             PlayerIds.Clear();
             KillClickCount.Clear();
         }
@@ -69,50 +63,66 @@ namespace TOHE.Roles.Neutral
                 Main.ResetCamPlayerList.Add(playerId);
         }
 
-         private void SendRPC(byte playerId, byte targetId)
-    {
-        MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.SyncRoleSkill, SendOption.Reliable, -1);
-        writer.WriteNetObject(_Player);
-        writer.Write(playerId);
-        writer.Write(AbilityLimit);
-        writer.Write(targetId);
-        AmongUsClient.Instance.FinishRpcImmediately(writer);
-    }
-
         public override void ApplyGameOptions(IGameOptions opt, byte id) => opt.SetVision(HasImpostorVision.GetBool());
-        public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
-        public override bool CanUseKillButton(PlayerControl pc) => true;
-        public override bool CanUseImpostorVentButton(PlayerControl pc) => CanVent.GetBool();
-        public override bool CanUseSabotage(PlayerControl pc) => false;
-        public void UseKillButton(PlayerControl killer, PlayerControl target)
-        {
 
-       public override bool OnCheckMurderAsKiller(PlayerControl killer, PlayerControl target)
-    {
-        if (AbilityLimit > 0)
+        public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
+
+        public override bool CanUseKillButton(PlayerControl pc) => true;
+
+        public override bool CanUseImpostorVentButton(PlayerControl pc) => CanVent.GetBool();
+
+        public override bool CanUseSabotage(PlayerControl pc) => false;
+
+        public override void UseKillButton(PlayerControl killer, PlayerControl target)
         {
-            return killer.CheckDoubleTrigger(target, () => { SetPainting(killer, target); });
+            if (target == null || !PlayerIds.Contains(killer.PlayerId))
+                return;
+
+            // Increment kill click count for the killer
+            KillClickCount[killer.PlayerId]++;
+
+            // If it's the first click, paint the player
+            if (KillClickCount[killer.PlayerId] == 1)
+            {
+                SetPainting(killer, target);
+            }
+            // If it's the second click, check murder as killer (which may result in a kill)
+            else if (KillClickCount[killer.PlayerId] == 2)
+            {
+                OnCheckMurderAsKiller(killer, target);
+            }
         }
-        else return true;
+
+        public override bool OnCheckMurderAsKiller(PlayerControl killer, PlayerControl target)
+        {
+            if (AbilityLimit > 0)
+            {
+                return killer.CheckDoubleTrigger(target, () => { SetPainting(killer, target); });
+            }
+            else
+            {
+                return true; // Default behavior, can be adjusted as needed
             }
         }
 
         private void SetPainting(PlayerControl killer, PlayerControl target)
         {
+            if (!PlayerSkinsPainted.ContainsKey(killer.PlayerId))
+            {
+                PlayerSkinsPainted[killer.PlayerId] = new List<byte>();
+            }
+
             if (!PlayerSkinsPainted[killer.PlayerId].Contains(target.PlayerId))
             {
                 SetSkin(target, PaintedOutfit);
+                PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
             }
 
-            PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
-            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("ArtistPaintedSkin")));
-            target.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("PaintedByArtist")));
-
-            OriginalPlayerSkins[target.PlayerId] = Camouflage.PlayerSkins[target.PlayerId];
-            Camouflage.PlayerSkins[target.PlayerId] = PaintedOutfit;
+            killer.Notify(ColorString(GetRoleColor(CustomRoles.Artist), GetString("ArtistPaintedSkin")));
+            target.Notify(ColorString(GetRoleColor(CustomRoles.Artist), GetString("PaintedByArtist")));
         }
 
-        private static void SetSkin(PlayerControl target, NetworkedPlayerInfo.PlayerOutfit outfit)
+        private void SetSkin(PlayerControl target, NetworkedPlayerInfo.PlayerOutfit outfit)
         {
             var sender = CustomRpcSender.Create(name: $"Artist.RpcSetSkin({target.Data.PlayerName})");
 
@@ -156,8 +166,7 @@ namespace TOHE.Roles.Neutral
         {
             if (target != null)
             {
-                // Logic to perform the kill
-                killer.Kill(target);
+                killer.MurderPlayer(target);
                 killer.Notify($"You have killed {target.Data.PlayerName}.");
             }
         }
