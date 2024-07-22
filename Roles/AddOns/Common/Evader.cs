c0182329e474b5eef062baf4f04c2a05e729bf89
Limeau <148590907+Limeau@users.noreply.github.com>
2024-07-22 16:51:58 -0600

    Reapply "Try fix evader *again*"
    This reverts commit e33f1863bd50d9d88fbfd84aca28e84d367c0016.

--- Roles/AddOns/Common/Evader.cs
+++ Roles/AddOns/Common/Evader.cs
@@ -1,55 +1,60 @@
 using static TOHE.Options;
 
-namespace TOHE.Roles.AddOns.Common;
-
-public static class Evader
+namespace TOHE.Roles.AddOns.Common
 {
-    private const int Id = 28600;
-
-    public static OptionItem ChanceToEvadeVote;
-    public static OptionItem ImpCanBeEvader;
-    public static OptionItem CrewCanBeEvader;
-    public static OptionItem NeutralCanBeEvader;
-
-
-    private static Dictionary<byte, bool> Evade;
-        
-    
-    public static void SetupCustomOptions()
+    public static class Evader
     {
-        SetupAdtRoleOptions(Id, CustomRoles.Evader, canSetNum: true);
-        ChanceToEvadeVote = IntegerOptionItem.Create(Id + 13, "ChanceToEvadeVote", new(0, 100, 5), 50, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader])
-            .SetValueFormat(OptionFormat.Percent);
-        ImpCanBeEvader = BooleanOptionItem.Create(Id + 10, "ImpCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
-        CrewCanBeEvader = BooleanOptionItem.Create(Id + 11, "CrewCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
-        NeutralCanBeEvader = BooleanOptionItem.Create(Id + 12, "NeutralCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
-    }
-    public static void Init()
-    {
-        Evade = [];
-    }
+        private const int Id = 28600;
 
-    private static void EvadeChance()
-    {
-        var rd = IRandom.Instance;
-        if (rd.Next(0, 101) < ChanceToEvadeVote.GetInt())
+        public static OptionItem ChanceToEvadeVote;
+        public static OptionItem ImpCanBeEvader;
+        public static OptionItem CrewCanBeEvader;
+        public static OptionItem NeutralCanBeEvader;
+
+        private static Dictionary<byte, bool> Evade;
+
+        public static void SetupCustomOptions()
         {
-            var evader = PlayerControl.LocalPlayer;
-            Evade[evader.PlayerId] = true;
+            SetupAdtRoleOptions(Id, CustomRoles.Evader, canSetNum: true);
+            ChanceToEvadeVote = IntegerOptionItem.Create(Id + 13, "ChanceToEvadeVote", new(0, 100, 5), 50, TabGroup.Addons, false)
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Evader])
+                .SetValueFormat(OptionFormat.Percent);
+            ImpCanBeEvader = BooleanOptionItem.Create(Id + 10, "ImpCanBeEvader", true, TabGroup.Addons, false)
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
+            CrewCanBeEvader = BooleanOptionItem.Create(Id + 11, "CrewCanBeEvader", true, TabGroup.Addons, false)
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
+            NeutralCanBeEvader = BooleanOptionItem.Create(Id + 12, "NeutralCanBeEvader", true, TabGroup.Addons, false)
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
         }
-    }
 
-    public static bool CheckRealVotes(PlayerControl target, ref int VoteNum)
-
-    {
-        EvadeChance();
-        if (Evade[target.PlayerId])
+        public static void Init()
         {
-            Evade[target.PlayerId] = false;
-
-            VoteNum = 0;
-            return false;
+            Evade = new Dictionary<byte, bool>();
         }
-        return true;
+
+        private static void EvadeChance()
+        {
+            var rd = IRandom.Instance;
+            if (rd.Next(0, 101) < ChanceToEvadeVote.GetInt())
+            {
+                var evader = PlayerControl.LocalPlayer;
+                if (evader != null)
+                {
+                    Evade[evader.PlayerId] = true;
+                }
+            }
+        }
+
+        public static bool CheckRealVotes(PlayerControl target, ref int VoteNum)
+        {
+            EvadeChance();
+            if (Evade.TryGetValue(target.PlayerId, out bool didEvade) && didEvade)
+            {
+                Evade[target.PlayerId] = false;
+                VoteNum = 0;
+                return false;
+            }
+            return true;
+        }
     }
 }
