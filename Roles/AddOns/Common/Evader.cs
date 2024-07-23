bad9330a2007f303e31ea76bbf804b5b7d1e7543
Dailyhare <156022382+Dailyhare@users.noreply.github.com>
2024-07-22 17:01:04 -0700

    Update Evader.cs

--- Roles/AddOns/Common/Evader.cs
+++ Roles/AddOns/Common/Evader.cs
@@ -1,50 +1,44 @@
 using static TOHE.Options;
 
-namespace TOHE.Roles.AddOns.Common;
-
-public static class Evader
+namespace TOHE.Roles.AddOns.Common
 {
-    private const int Id = 28600;
+    public static class Evader
+    {
+        private const int Id = 28600;
 
-    public static OptionItem ChanceToEvadeVote;
-    public static OptionItem ImpCanBeEvader;
-    public static OptionItem CrewCanBeEvader;
-    public static OptionItem NeutralCanBeEvader;
+        public static OptionItem ChanceToEvadeVote;
+        public static OptionItem ImpCanBeEvader;
+        public static OptionItem CrewCanBeEvader;
+        public static OptionItem NeutralCanBeEvader;
 
+        private static Dictionary<byte, bool> Evade;
 
-    private static Dictionary<byte, bool> Evade;
+        public static void SetupCustomOptions()
+        {
+            SetupAdtRoleOptions(Id, CustomRoles.Evader, canSetNum: true);
+            ChanceToEvadeVote = IntegerOptionItem.Create(Id + 13, "ChanceToEvadeVote", new(0, 100, 5), 50, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader])
+                .SetValueFormat(OptionFormat.Percent);
+            ImpCanBeEvader = BooleanOptionItem.Create(Id + 10, "ImpCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
+            CrewCanBeEvader = BooleanOptionItem.Create(Id + 11, "CrewCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
+            NeutralCanBeEvader = BooleanOptionItem.Create(Id + 12, "NeutralCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
+        }
         
-    
-    public static void SetupCustomOptions()
-    {
-        SetupAdtRoleOptions(Id, CustomRoles.Evader, canSetNum: true);
-        ChanceToEvadeVote = IntegerOptionItem.Create(Id + 13, "ChanceToEvadeVote", new(0, 100, 5), 50, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader])
-            .SetValueFormat(OptionFormat.Percent);
-        ImpCanBeEvader = BooleanOptionItem.Create(Id + 10, "ImpCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
-        CrewCanBeEvader = BooleanOptionItem.Create(Id + 11, "CrewCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
-        NeutralCanBeEvader = BooleanOptionItem.Create(Id + 12, "NeutralCanBeEvader", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Evader]);
-    }
-    public static void Init()
-    {
-        Evade = new Dictionary<byte, bool>();
-    }
-
-    private static void EvadeChance()
-    {
-        var rd = IRandom.Instance;
-        if (rd.Next(0, 101) < ChanceToEvadeVote.GetInt())
+        public static void Init()
         {
-            var evader = PlayerControl.LocalPlayer;
-            Evade[evader.PlayerId] = true;
-        }
-    }
-    public static void EvadeVote(byte playerId)
-        {
-            Evade[playerId] = true;
+            Evade = new Dictionary<byte, bool>();
         }
 
-    
-    public static bool CheckRealVotes(PlayerControl target, ref int VoteNum)
+        private static void EvadeChance()
+        {
+            var rd = IRandom.Instance;
+            if (rd.Next(0, 101) < ChanceToEvadeVote.GetInt())
+            {
+                var evader = PlayerControl.LocalPlayer;
+                Evade[evader.PlayerId] = true;
+            }
+        }
+
+        public static bool CheckRealVotes(PlayerControl target, ref int VoteNum)
         {
             EvadeChance();
             if (Evade.ContainsKey(target.PlayerId) && Evade[target.PlayerId])
@@ -55,4 +49,5 @@ public static class Evader
             }
             return true;
         }
+    }
 }
