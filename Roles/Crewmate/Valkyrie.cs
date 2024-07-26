d2f89e44c19e9c7031abce4b238733056ad010d1
Limeau <148590907+Limeau@users.noreply.github.com>
2024-07-26 11:03:15 -0600

    Nerf in Valkyrie settings

--- Roles/Crewmate/Valkyrie.cs
+++ Roles/Crewmate/Valkyrie.cs
@@ -30,7 +30,7 @@ internal class Valkyrie : RoleBase
    SetupSingleRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Valkyrie, 1, zeroOne: false);
         KillCooldown = FloatOptionItem.Create(Id + 10, GeneralOption.KillCooldown, new(300f, 300f, 300f), 300f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie])
             .SetValueFormat(OptionFormat.Seconds);
-        RevengeTime = IntegerOptionItem.Create(Id + 11, "ValkyrieRevengeTime", new(0, 60, 1), 30, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie])
+        RevengeTime = IntegerOptionItem.Create(Id + 11, "ValkyrieRevengeTime", new(0, 30, 5), 10, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie])
             .SetValueFormat(OptionFormat.Seconds);
         CanVent = BooleanOptionItem.Create(Id + 12, GeneralOption.CanVent, true, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie]);
     }
