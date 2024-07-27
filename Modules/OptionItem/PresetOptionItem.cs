8821350a648ba1558de2bf96eb751974a604c475
AndriesWorks <andries46565@gmail.com>
2024-07-27 21:55:49 +0200

    Revert "Update PresetOptionItem.cs"
    This reverts commit dcdb3f6b089748e90dab133ebfb2a0eb36deafca.

--- Modules/OptionItem/PresetOptionItem.cs
+++ Modules/OptionItem/PresetOptionItem.cs
@@ -1,4 +1,4 @@
-namespace TOHO;
+namespace TOHE;
 
 public class PresetOptionItem(int defaultValue, TabGroup tab, bool vanilla) : OptionItem(0, "Preset", defaultValue, tab, true, vanillaStr:vanilla)
 {
